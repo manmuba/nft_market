f5c0314ade26fd565a3eda73e68e079fd3af0f1f
Mubarek Shikur <manmuba90@gmail.com>
2023-07-01 07:39:31 +0300

    Registration page with views competed successfully

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth
 
+from .models import Account
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        first_name  = request.POST['first_name']
+        last_name   = request.POST['last_name']
+        email       = request.POST['email']
+        password    = request.POST['password']
+        if Account.objects.filter(email=email).count() == 0:
+            user = Account.objects.create_user(
+                first_name  = first_name,
+                last_name   = last_name,
+                email       = email,
+                username    = email.split('@')[0]
+            )
+            user.set_password(password)
+            user.is_active = True
+            user.save()
+            auth.login(request, user)
+            return redirect('Home')
+    return render(request, 'auth/register.html')
--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 # Media file and Static Configuraton
@@ -9,6 +9,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.HomePage, name="Home"),
+    path('auth/', include('accounts.urls')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
