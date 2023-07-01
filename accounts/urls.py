fbdbf3febbf1cdb0af0ac9d9bfc98c7786a95f16
Mubarek Shikur <manmuba90@gmail.com>
2023-07-01 11:44:41 +0300

    Reset password competed successfully

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('activate/<uidb64>/<token>/', views.activate, name="activate"),
     path('logout/', views.logout, name="logout"),
-    path('edit_profile/', views.edit_profile, name='edit_profile')
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
+
+    path('forget_password/', views.forget_password, name='forget_password'),
+    path('resetpassword_validate/<uidb64>/<token>/', views.resetpassword_validate, name='resetpassword_validate'),
+    path('reset_password/', views.reset_password, name='reset_password'),
 ]
